31f3a5778ce3b93045206e6666847a3b586edec2
duong <beabyduong@gmail.com>
2025-05-17 09:30:34 +0700

    Chỉnh sửa màu button

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
+// const slider = document.querySelector(".slider");
+// const dots = document.querySelectorAll(".dots span");
+// const totalSlides = dots.length;
+
+// let currentIndex = 0;
+
+// function updateSlider(index) {
+//   slider.style.transform = `translateX(-${index * 100}vw)`;
+//   dots.forEach((dot) => dot.classList.remove("active"));
+//   dots[index].classList.add("active");
+//   currentIndex = index;
+// }
+
+// dots.forEach((dot) => {
+//   dot.addEventListener("click", () => {
+//     updateSlider(Number(dot.dataset.index));
+//   });
+// });
+
+// // Swipe touch support
+// let startX = 0;
+// slider.addEventListener("touchstart", (e) => {
+//   startX = e.touches[0].clientX;
+// });
+
+// slider.addEventListener("touchend", (e) => {
+//   const endX = e.changedTouches[0].clientX;
+//   const diff = endX - startX;
+//   if (Math.abs(diff) > 50) {
+//     if (diff > 0) {
+//       // swipe right
+//       const index = (currentIndex - 1 + totalSlides) % totalSlides;
+//       updateSlider(index);
+//     } else {
+//       // swipe left
+//       const index = (currentIndex + 1) % totalSlides;
+//       updateSlider(index);
+//     }
+//   }
+// });
+
+// // Set mặc định
+// updateSlider(0);
 const slider = document.querySelector(".slider");
 const dots = document.querySelectorAll(".dots span");
 const totalSlides = dots.length;
 
 let currentIndex = 0;
+let autoSlideInterval;
 
 function updateSlider(index) {
   slider.style.transform = `translateX(-${index * 100}vw)`;
@@ -11,9 +55,20 @@ function updateSlider(index) {
   currentIndex = index;
 }
 
+// Auto slide mỗi 5 giây
+function startAutoSlide() {
+  autoSlideInterval = setInterval(() => {
+    const nextIndex = (currentIndex + 1) % totalSlides;
+    updateSlider(nextIndex);
+  }, 5000);
+}
+
+// Dừng và khởi động lại khi người dùng click
 dots.forEach((dot) => {
   dot.addEventListener("click", () => {
+    clearInterval(autoSlideInterval);
     updateSlider(Number(dot.dataset.index));
+    startAutoSlide();
   });
 });
 
@@ -28,16 +83,17 @@ slider.addEventListener("touchend", (e) => {
   const diff = endX - startX;
   if (Math.abs(diff) > 50) {
     if (diff > 0) {
-      // swipe right
       const index = (currentIndex - 1 + totalSlides) % totalSlides;
       updateSlider(index);
     } else {
-      // swipe left
       const index = (currentIndex + 1) % totalSlides;
       updateSlider(index);
     }
+    clearInterval(autoSlideInterval);
+    startAutoSlide();
   }
 });
 
-// Set mặc định
+// Khởi tạo ban đầu
 updateSlider(0);
+startAutoSlide();
